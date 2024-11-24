c7edbb651b830541479f380fe3493f2114497d7e
Geronimoserial <serialgeronimo@gmail.com>
2024-11-23 23:02:02 -0300

    test

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -28,17 +28,17 @@ export const Navigation: React.FC = () => {
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
 					<div className="flex justify-between gap-8">
-						<Link
+						{/* <Link
 							href="/projects"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
 							Projects
-						</Link>
+						</Link> */}
 						<Link
-							href="/contact"
+							href="/"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
-							Contact
+							Home
 						</Link>
 					</div>
 
