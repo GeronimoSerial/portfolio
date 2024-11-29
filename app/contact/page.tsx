faac753d102bf6b8b05ec88453828866da0904f4
Geronimo Serial <serialgeronimo@gmail.com>
2024-11-28 21:22:35 -0300

    Update responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -49,32 +49,31 @@ export default function Contact() {
 		<div className="min-h-screen bg-gradient-to-tl from-indigo-900 via-indigo-400/10 fixed inset-0">
 		<Navigation/>
 		<Particles className='absolute inset-0 -z-10'/>
-		<div className="fixed inset-0 flex items-center justify-center">
-		  <div className="relative w-full max-w-[90vw] lg:max-w-[85vw] xl:max-w-[80vw] mx-auto">
+		<div className="fixed inset-0 flex items-center justify-center p-4">
+		  <div className="w-full max-w-[95vw] md:max-w-[90vw] lg:max-w-[85vw] xl:max-w-[80vw] mx-auto">
 			<Carousel
 			  opts={{
 				  align: "center",
 				  loop: true,
 				  dragFree: false,
-				  containScroll: "trimSnaps",
 				}}
-				className="w-full px-4 sm:px-6 md:px-8"
+				className="w-full"
 				>
 			  <CarouselContent className="-ml-4 md:-ml-6">
 				{socials.map((social, index) => (
 					<CarouselItem 
 					key={index} 
-					className="pl-4 md:pl-6 basis-full xs:basis-2/3 sm:basis-1/2 md:basis-1/3"
+					className="pl-4 md:pl-6 basis-full sm:basis-1/2 md:basis-1/3"
 					>
-					<div className="flex justify-center items-center h-full">
+					<div className="flex justify-center items-center h-full p-2 md:p-3">
 					  <SocialCard {...social} />
 					</div>
 				  </CarouselItem>
 				))}
 			  </CarouselContent>
-			  <div className="hidden sm:block">
-				<CarouselPrevious className="absolute -left-12 lg:-left-16 transform -translate-y-1/2 top-1/2" />
-				<CarouselNext className="absolute -right-12 lg:-right-16 transform -translate-y-1/2 top-1/2" />
+			  <div className="sm:block hidden">
+				<CarouselPrevious className="absolute -left-4 md:-left-8 lg:-left-12 transform -translate-y-1/2 top-1/2" />
+				<CarouselNext className="absolute -right-4 md:-right-8 lg:-right-12 transform -translate-y-1/2 top-1/2" />
 			  </div>
 			</Carousel>
 		  </div>
