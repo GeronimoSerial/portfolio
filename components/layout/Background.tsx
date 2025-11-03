e818a59d7e9c46f6c9dd982e66f6f0c114059d2f
Geronimo Serial <serialgeronimo@gmail.com>
2025-11-03 09:42:54 -0300

    refactor: Update Background component styles for improved visibility and responsiveness

--- components/layout/Background.tsx
+++ components/layout/Background.tsx
@@ -1,26 +1,3 @@
-// import { cn } from "@/lib/utils";
-// import React from "react";
-
-// export function Background({ children }: { children: React.ReactNode }) {
-//   return (
-//     <>
-//       <div className="fixed inset-0 -z-10 h-full w-full bg-white dark:bg-black">
-//         <div
-//           className={cn(
-//             "absolute inset-0",
-//             "[background-size:20px_20px]",
-//             "bg-[image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
-//             "dark:bg-[image:radial-gradient(#404040_1px,transparent_1px)]"
-//           )}
-//         />
-//         {/* Radial gradient for the container to give a faded look */}
-//         <div className="pointer-events-none absolute inset-0 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
-//       </div>
-//       {children}
-//     </>
-//   );
-// }
-
 import { cn } from "@/lib/utils";
 import React from "react";
 
@@ -31,7 +8,7 @@ export function Background({ children }: { children: React.ReactNode }) {
         <div
           className={cn(
             "absolute inset-0",
-            "opacity-0 md:opacity-90",
+            "opacity-100",
             "[background-size:40px_40px]",
             "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
             "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
